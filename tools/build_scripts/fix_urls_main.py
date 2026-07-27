5083f2aed50bd5384c7a12d415b032779ee846a8
Gxxx <rappernx@gmail.com>
2026-07-28 01:19:26 +0700

    Switch to main branch in URLs

---
+++ tools/build_scripts/fix_urls_main.py
@@ -0,0 +1,11 @@
+﻿import json
+
+with open('plugin.json', 'r', encoding='utf-8') as f:
+    repo_data = json.load(f)
+
+for item in repo_data['data']:
+    item['path'] = item['path'].replace('/master/', '/main/')
+    item['icon'] = item['icon'].replace('/master/', '/main/')
+
+with open('plugin.json', 'w', encoding='utf-8') as f:
+    json.dump(repo_data, f, indent=2, ensure_ascii=False)
