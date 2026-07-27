5c8f63807a24875e1fda517fcdb03fbf73f32f92
Gxxx <rappernx@gmail.com>
2026-07-28 01:14:34 +0700

    Switch to GitHack CDN and bump to V5 to completely bypass cache issues

--- tools/build_scripts/fix_urls.py
+++ tools/build_scripts/fix_urls.py
@@ -1,11 +1,14 @@
 ﻿import json
-import os
 
 with open('plugin.json', 'r', encoding='utf-8') as f:
     repo_data = json.load(f)
 
-repo_data['data'][0]['path'] = "https://cdn.jsdelivr.net/gh/gx288/my-vbook-repo@master/truyenc/plugin.zip"
-repo_data['data'][0]['icon'] = "https://cdn.jsdelivr.net/gh/gx288/my-vbook-repo@master/truyenc/icon.png"
+for item in repo_data['data']:
+    if 'jsdelivr.net' in item['path']:
+        item['path'] = item['path'].replace('cdn.jsdelivr.net/gh/gx288/my-vbook-repo@master', 'raw.githack.com/gx288/my-vbook-repo/master')
+        item['icon'] = item['icon'].replace('cdn.jsdelivr.net/gh/gx288/my-vbook-repo@master', 'raw.githack.com/gx288/my-vbook-repo/master')
+        # Ensure version is bumped to 5 to trigger vBook update
+        item['version'] = 5
 
 with open('plugin.json', 'w', encoding='utf-8') as f:
     json.dump(repo_data, f, indent=2, ensure_ascii=False)
